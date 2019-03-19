5e483c345aa0535edc41374846324d7214d181cf
robin.hermanek <robin.hermanek@fhnw.ch>
2019-03-19 19:55:59 +0100

    Basic Crud implementation

--- src/main/java/vociapp/vociController.kt
+++ src/main/java/vociapp/vociController.kt
@@ -1,22 +1,22 @@
 package vociapp
 
 
-class VociController(val vociModel: Model) : Controller {
+class VociController(val vociModel: VociFunctions) : Controller {
     override fun execute(command: String): Boolean {
 
         if (command.startsWith("test")) {
             if (command.endsWith("francais")){
-                vociModel.testfrench()
+                vociModel.testFrench()
             } else if (command.endsWith("english")){
-                vociModel.testenglish()
+                vociModel.testEnglish()
                 println("Test")
             }
             return true
         } else if (command.startsWith("add ") && parseCommand(command) == "f") {
-            vociModel.addfrench(getGerman(command), getSecondLanguage(command))
+            vociModel.addFrench(getGerman(command), getSecondLanguage(command))
             return true
         } else if (command.startsWith("add ") && parseCommand(command) == "e") {
-            vociModel.addenglish(getGerman(command), getSecondLanguage(command))
+            vociModel.addEnglish(getGerman(command), getSecondLanguage(command))
             return true
         }
 
